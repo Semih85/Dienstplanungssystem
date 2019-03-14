fdd2e335d259318130fd70002009a07a6ca07fa6
Semih ÖZDAMAR <ozdamarsem85@hotmail.com>
2019-03-14 23:46:49 +0300

    console ui denendi

---
+++ WM.UI.Console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WM.Northwind.Business.Abstract.EczaneNobet;
+using WM.Northwind.Business.Abstract.Optimization.EczaneNobet;
+
+namespace WM.UI.Console
+{
+    class Program
+    {
+        //private static IAntalyaMerkezOptimizationService _antalyaMerkezOptimizationService;
+        //private static INobetGrupGorevTipService _nobetGrupGorevTipService;
+
+        //public Program(INobetGrupGorevTipService nobetGrupGorevTipService)
+        //{
+        //    _nobetGrupGorevTipService = nobetGrupGorevTipService;
+        //}
+
+        static void Main(string[] args)
+        {
+            // var a =  _nobetGrupGorevTipService.GetDetaylar(1);
+            //Console.Writeline("");
+            Console.WriteLine(c);
+        }
+    }
+}
