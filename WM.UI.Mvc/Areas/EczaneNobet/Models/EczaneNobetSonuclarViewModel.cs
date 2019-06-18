96bc60a1d834647f4659ec9032d0c18893f682bf
Semih ÖZDAMAR <ozdamarsem85@hotmail.com>
2019-06-18 05:27:02 +0300

    AyniGunNobetTutanEczane nesnesi yerine AyniGunTutulanNobetDetay nesnesi kullanılmaya başlandı.

--- WM.UI.Mvc/Areas/EczaneNobet/Models/EczaneNobetSonuclarViewModel.cs
+++ WM.UI.Mvc/Areas/EczaneNobet/Models/EczaneNobetSonuclarViewModel.cs
@@ -11,13 +11,13 @@ namespace WM.UI.Mvc.Areas.EczaneNobet.Models
         public List<EczaneNobetIstatistikGunFarki> GunFarklariTumSonuclar { get; set; }
         public List<EczaneNobetIstatistikGunFarkiFrekans> GunFarklariFrekanslar { get; set; }
         public List<EsGrubaAyniGunYazilanNobetler> EsGrubaAyniGunYazilanNobetler { get; internal set; }
-        public List<AyniGunNobetTutanEczane> AltGrupAyniGunNobetTutanEczaneler { get; internal set; }
+        public List<AyniGunTutulanNobetDetay> AltGrupAyniGunNobetTutanEczaneler { get; internal set; }
         public List<EczaneNobetAlacakVerecek> EczaneNobetAlacakVerecek { get; internal set; }
         //AltGrupIleAyniGunNobetTutanEczane
         public DateTime? BaslangicTarihi { get; internal set; }
         public DateTime? BitisTarihi { get; internal set; }
         public EczaneNobetSonucModel EczaneNobetSonuclar { get; internal set; }
-        public List<AyniGunNobetTutanEczane> AyniGunNobetTutanEczaneler { get; internal set; }
+        public List<AyniGunTutulanNobetDetay> AyniGunNobetTutanEczaneler { get; internal set; }
         public List<NobetGrupGunDagilim> GunDagilimiMaxMin { get; internal set; }
         public int NobetUstGrupId { get; set; }
         public List<RaporDetay> Raporlar { get; internal set; }
