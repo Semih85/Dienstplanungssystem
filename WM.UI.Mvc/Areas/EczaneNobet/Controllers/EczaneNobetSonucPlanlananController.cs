533822624c343a3aa4fa32e2c631a3cf982953cf
Semih ÖZDAMAR <ozdamarsem85@hotmail.com>
2019-06-23 23:55:29 +0300

    Nakil eczaneye göre planlanan nöbet yazdırma arayüzleri eklendi

--- WM.UI.Mvc/Areas/EczaneNobet/Controllers/EczaneNobetSonucPlanlananController.cs
+++ WM.UI.Mvc/Areas/EczaneNobet/Controllers/EczaneNobetSonucPlanlananController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using WM.Northwind.Business.Abstract.EczaneNobet;
+using WM.Northwind.Entities.ComplexTypes.EczaneNobet;
 using WM.Northwind.Entities.Concrete.EczaneNobet;
 using WM.UI.Mvc.Models;
 using WM.UI.Mvc.Services;
@@ -15,25 +16,30 @@ namespace WM.UI.Mvc.Areas.EczaneNobet.Controllers
 {
     public class EczaneNobetSonucPlanlananController : Controller
     {
+        #region ctor
         private WMUIMvcContext db = new WMUIMvcContext();
         private IEczaneNobetSonucPlanlananService _eczaneNobetSonucPlanlananService;
         private INobetUstGrupSessionService _nobetUstGrupSessionService;
         private INobetGrupGorevTipService _nobetGrupGorevTipService;
         private ITakvimService _takvimService;
         private IEczaneNobetGrupService _eczaneNobetGrupService;
+        private INobetUstGrupGunGrupService _nobetUstGrupGunGrupService;
 
         public EczaneNobetSonucPlanlananController(IEczaneNobetSonucPlanlananService eczaneNobetSonucPlanlananService,
             INobetUstGrupSessionService nobetUstGrupSessionService,
             INobetGrupGorevTipService nobetGrupGorevTipService,
             ITakvimService takvimService,
-            IEczaneNobetGrupService eczaneNobetGrupService)
+            IEczaneNobetGrupService eczaneNobetGrupService,
+            INobetUstGrupGunGrupService nobetUstGrupGunGrupService)
         {
             _eczaneNobetSonucPlanlananService = eczaneNobetSonucPlanlananService;
             _nobetUstGrupSessionService = nobetUstGrupSessionService;
             _nobetGrupGorevTipService = nobetGrupGorevTipService;
             _takvimService = takvimService;
             _eczaneNobetGrupService = eczaneNobetGrupService;
+            _nobetUstGrupGunGrupService = nobetUstGrupGunGrupService;
         }
+        #endregion
 
         // GET: EczaneNobet/EczaneNobetSonucPlanlanan
         public ActionResult Index()
@@ -43,7 +49,22 @@ namespace WM.UI.Mvc.Areas.EczaneNobet.Controllers
             var nobetGrupGorevTipler = _nobetGrupGorevTipService.GetMyDrop(nobetUstGrup.Id);
 
             ViewBag.NobetGrupGorevTipId = new SelectList(items: nobetGrupGorevTipler, dataValueField: "Id", dataTextField: "Value");
-            //var eczaneNobetSonucPlanlananlar = _eczaneNobetSonucPlanlananService.GetDetaylar(nobetUstGrup.Id);
+
+            return View();
+        }
+
+        public ActionResult IndexNakilEczaneyeGore()
+        {
+            var nobetUstGrup = _nobetUstGrupSessionService.GetNobetUstGrup();
+
+            var nobetGrupGorevTipler = _nobetGrupGorevTipService.GetMyDrop(nobetUstGrup.Id);
+
+            var nobetUstGrupGunGruplar = _nobetUstGrupGunGrupService.GetDetaylar(nobetUstGrup.Id);
+
+            var gunGruplar = nobetUstGrupGunGruplar.Select(s => new MyDrop { Id = s.GunGrupId, Value = s.GunGrupAdi }).Distinct().ToList();
+
+            ViewBag.NobetGrupGorevTipId = new SelectList(items: nobetGrupGorevTipler, dataValueField: "Id", dataTextField: "Value");
+            ViewBag.GunGrupId = new SelectList(items: gunGruplar, dataValueField: "Id", dataTextField: "Value");
 
             return View();
         }
@@ -54,13 +75,13 @@ namespace WM.UI.Mvc.Areas.EczaneNobet.Controllers
             return PartialView();
         }
 
-        public JsonResult PlanlanaNobetleriYaz(DateTime baslangicTarihi, DateTime bitisTarihi, int nobetGrupGorevTipId)
+        public JsonResult PlanlanaNobetleriYaz(DateTime? baslangicTarihi, DateTime bitisTarihi, int nobetGrupGorevTipId = 0)
         {
             //baslangicTarihi = new DateTime(2018, 6, 1);
             //baslangicTarihi = new DateTime(2019, 3, 13);
             //bitisTarihi = new DateTime(2020, 12, 31);
 
-            var eczaneNobetGruplarHepsi = _eczaneNobetGrupService.GetDetaylarByNobetGrupGorevTipler(nobetGrupGorevTipId);//, baslangicTarihi, bitisTarihi);
+            //var eczaneNobetGruplarHepsi = _eczaneNobetGrupService.GetDetaylarByNobetGrupGorevTipler(nobetGrupGorevTipId);//, baslangicTarihi, bitisTarihi);
 
             //var nobetGrupGorevTipler = _nobetGrupGorevTipService.GetDetaylar(nobetGrupIdListe.ToList());
 
@@ -70,11 +91,65 @@ namespace WM.UI.Mvc.Areas.EczaneNobet.Controllers
 
             //var besinciBolge = nobetGrupGorevTipler.SingleOrDefault(x => x.Id == 8);
 
-            var nobetGrupGorevTip = _nobetGrupGorevTipService.GetDetayById(nobetGrupGorevTipId);
+            //var nobetGrupGorevTip = _nobetGrupGorevTipService.GetDetayById(nobetGrupGorevTipId);
 
-            _takvimService.SiraliNobetYazGrupBazinda(nobetGrupGorevTip, eczaneNobetGruplarHepsi, baslangicTarihi, bitisTarihi);
+            var nobetUstGrup = _nobetUstGrupSessionService.GetNobetUstGrup();
 
-            var jsonResult = Json(nobetGrupGorevTip, JsonRequestBehavior.AllowGet);
+            var nobetGrupGorevTipler = _nobetGrupGorevTipService.GetDetaylar(nobetUstGrup.Id)
+                .Where(w => w.Id == nobetGrupGorevTipId || nobetGrupGorevTipId == 0).ToList();
+
+            foreach (var nobetGrupGorevTip in nobetGrupGorevTipler)
+            {
+                var baslamaTarihi = nobetGrupGorevTip.BaslamaTarihi;
+
+                var eczaneNobetGruplarHepsi = _eczaneNobetGrupService.GetDetaylarByNobetGrupGorevTipler(nobetGrupGorevTip.Id);
+
+                if (baslangicTarihi != null)
+                {
+                    baslamaTarihi = (DateTime)baslangicTarihi;
+                }
+
+                _takvimService.SiraliNobetYazGrupBazinda(nobetGrupGorevTip, eczaneNobetGruplarHepsi, baslamaTarihi, bitisTarihi);
+            }
+
+            var jsonResult = Json(nobetGrupGorevTipler.Count(), JsonRequestBehavior.AllowGet);
+
+            //jsonResult.MaxJsonLength = int.MaxValue;
+
+            return jsonResult;
+        }
+
+        public JsonResult PlanlanaNobetleriYazNakilEczaneyeGore(DateTime? baslangicTarihi, DateTime bitisTarihi, int gunGrupId = 0, int nobetGrupGorevTipId = 0)
+        {
+            //baslangicTarihi = new DateTime(2018, 6, 1);
+            //baslangicTarihi = new DateTime(2019, 3, 13);
+            //bitisTarihi = new DateTime(2020, 12, 31);
+            var nobetUstGrup = _nobetUstGrupSessionService.GetNobetUstGrup();
+
+            var gunGruplar = _nobetUstGrupGunGrupService.GetDetaylar(nobetUstGrup.Id)
+                .Where(w => w.GunGrupId == gunGrupId || gunGrupId == 0).ToList();
+
+            var nobetGrupGorevTipler = _nobetGrupGorevTipService.GetDetaylar(nobetUstGrup.Id)
+                .Where(w => w.Id == nobetGrupGorevTipId || nobetGrupGorevTipId == 0).ToList();
+
+            foreach (var nobetGrupGorevTip in nobetGrupGorevTipler)
+            {
+                var baslamaTarihi = nobetGrupGorevTip.BaslamaTarihi;
+
+                if (baslangicTarihi != null)
+                {
+                    baslamaTarihi = (DateTime)baslangicTarihi;
+                }
+
+                var eczaneNobetGruplarHepsi = _eczaneNobetGrupService.GetDetaylarByNobetGrupGorevTipler(nobetGrupGorevTip.Id);
+
+                foreach (var gunGrup in gunGruplar)
+                {
+                    _takvimService.SiraliNobetYazGunGrupBazinda(nobetGrupGorevTip, eczaneNobetGruplarHepsi, baslamaTarihi, bitisTarihi, gunGrup.GunGrupId);
+                }
+            }
+
+            var jsonResult = Json(nobetGrupGorevTipler.Count(), JsonRequestBehavior.AllowGet);
 
             //jsonResult.MaxJsonLength = int.MaxValue;
 
