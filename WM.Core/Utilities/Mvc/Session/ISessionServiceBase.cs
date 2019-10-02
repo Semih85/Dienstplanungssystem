484376d424a007dc10c77882cb4b74f176a2c675
Semih ÖZDAMAR <ozdamarsem85@hotmail.com>
2019-10-02 21:31:01 +0200

    session nesnesi core'a eklendi

---
+++ WM.Core/Utilities/Mvc/Session/ISessionServiceBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WM.Core.Utilities.Mvc.Session
+{
+    public interface ISessionServiceBase<T> where T : class, new()
+    {
+        T GetSession(string sessionAdi);
+        //T GetSession();
+        void SetSession(T nobetUstGrupDetay, string sessionAdi);
+    }
+}
