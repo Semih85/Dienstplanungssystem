8eb5b2ce4cc3a6b34bd53a8e609979cddb0862b4
Semih ÖZDAMAR <ozdamarsem85@hotmail.com>
2019-10-04 15:36:44 +0200

    kısıt tanım kısa alanı eklendi

--- WM.Northwind.Entities/ComplexTypes/EczaneNobet/NobetUstGrupKisitDetay.cs
+++ WM.Northwind.Entities/ComplexTypes/EczaneNobet/NobetUstGrupKisitDetay.cs
@@ -12,27 +12,51 @@ namespace WM.Northwind.Entities.ComplexTypes.EczaneNobet
 {
     public class NobetUstGrupKisitDetay : IComplexType, ICloneable
     {
+        string AdDuzenle(string kisitAdiGosterilen)
+        {
+            var ilgiliKisitlar = new int[] { 23,38,43
+                ,57,75,76,77,78,79,80,81,82,83,20,21,34};
+
+            if (ilgiliKisitlar.Contains(KisitId))
+            {
+                kisitAdiGosterilen = kisitAdiGosterilen.Substring(3);
+            }
+
+            return kisitAdiGosterilen;
+        }
+
         public int Id { get; set; }
         public int NobetUstGrupId { get; set; }
         public int KisitId { get; set; }
+
         [Display(Name = "Sağ Taraf Değeri")]
         public double SagTarafDegeri { get; set; }
+
         [Display(Name = "Sağ Taraf Değeri Varsayılan")]
         public double SagTarafDegeriVarsayilan { get; set; }
+
         public bool PasifMi { get; set; }
         public bool VarsayilanPasifMi { get; set; }
+
         [Display(Name = "Nöbet Üst Grubu")]
         public string NobetUstGrupAdi { get; set; }
+
         [Display(Name = "Kısıt")]
         public string KisitAdi { get; set; }
-        [Display(Name = "Kısıt Adi")]
+
+        [Display(Name = "Kısıt Adı")]
         public string KisitAdiGosterilen { get; set; }
+
+        [Display(Name = "Kısıt Adi Kısa")]
+        public string KisitAdiGosterilenKisa => AdDuzenle(KisitAdiGosterilen);
+
         [Display(Name = "Kısıt Açıklama")]
         public string KisitAciklama { get; set; }
+
         [Display(Name = "Kısıt Kategori")]
         public string KisitKategoriAdi { get; set; }
 
-        public string KisitOnEk => KisitId < 10
+        public string KisitKodu => KisitId < 10
             ? $"K0{KisitId}"
             : $"K{KisitId}";
 
@@ -41,10 +65,14 @@ namespace WM.Northwind.Entities.ComplexTypes.EczaneNobet
             : "";
 
         public string KisitTanim => KisitId > 0
-            ? $"{KisitOnEk}, {KisitKategorisi}, {KisitAdiGosterilen},"
+            ? $"{KisitKodu}, {KisitKategorisi}, {KisitAdiGosterilen},"
             : "";
 
-        public string KisitAdiUzun => $"{KisitOnEk} ({KisitKategorisi}, {KisitAdiGosterilen})";
+        public string KisitTanimKisa => KisitId > 0
+            ? $"{KisitKodu}, {KisitKategorisi}, {KisitAdiGosterilenKisa},"
+            : "";
+
+        public string KisitAdiUzun => $"{KisitKodu} ({KisitKategorisi}, {KisitAdiGosterilen})";
 
         public int KisitKategoriId { get; set; }
         public int NobetGrupGorevtipKisitSayisi { get; set; }
