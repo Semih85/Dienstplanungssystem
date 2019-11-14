dab4c48caa89058ba898fd7e2b30f95a75cf24d4
Semih ÖZDAMAR <ozdamarsem85@hotmail.com>
2019-11-14 09:51:19 +0100

    tarih seçtirerek nöbet ekleme eklendi

---
+++ WM.Northwind.Entities/ComplexTypes/EczaneNobet/EczaneNobetSonucEkle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WM.Core.Entities;
+using WM.Northwind.Entities.Concrete.EczaneNobet;
+
+namespace WM.Northwind.Entities.ComplexTypes.EczaneNobet
+{
+    public class EczaneNobetSonucEkle : IComplexType
+    {
+        public int Id { get; set; }
+        public int EczaneNobetGrupId { get; set; }
+        public int NobetGorevTipId { get; set; }
+        public int TakvimId { get; set; }
+        public bool YayimlandiMi { get; set; }
+        public DateTime NobetTarihi { get; set; }
+    }
+}
+
