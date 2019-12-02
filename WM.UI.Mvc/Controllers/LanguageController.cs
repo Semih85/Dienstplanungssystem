5082b2d26f93bb0c17025e19b0327c939b5afe2e
Semih ÖZDAMAR <ozdamarsem85@hotmail.com>
2019-12-02 15:25:18 +0100

    dil seçenekleri eklendi.

---
+++ WM.UI.Mvc/Controllers/LanguageController.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WM.UI.Mvc.Controllers
+{
+    public class LanguageController : Controller
+    {
+        public ActionResult Change(string language, string viewName)
+        {           
+            if (language != null)
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+
+            HttpCookie cookie = new HttpCookie("language")
+            {
+                Value = language
+            };
+
+            Response.Cookies.Add(cookie);
+
+            //return RedirectToAction("Index", "Home");
+            return View(viewName);
+        }
+    }
+}
--- WM.UI.Mvc/Global.asax.cs
+++ WM.UI.Mvc/Global.asax.cs
@@ -72,5 +72,20 @@ namespace WM.UI.Mvc
             }
         }
 
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            HttpCookie cookie = HttpContext.Current.Request.Cookies["Language"];
+            if (cookie != null && cookie.Value != null)
+            {
+                System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(cookie.Value);
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(cookie.Value);
+            }
+            else
+            {
+                System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en");
+                System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en");
+            }
+        }
+
     }
 }
