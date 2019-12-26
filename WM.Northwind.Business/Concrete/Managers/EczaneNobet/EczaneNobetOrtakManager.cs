5edd2c058313dc03e242bd3cafcd85209e41d265
Semih ÖZDAMAR <ozdamarsem85@hotmail.com>
2019-12-26 21:02:19 +0100

    gereksiz kütüphaneler kaldırıldı

--- WM.Northwind.Business/Concrete/Managers/EczaneNobet/EczaneNobetOrtakManager.cs
+++ WM.Northwind.Business/Concrete/Managers/EczaneNobet/EczaneNobetOrtakManager.cs
@@ -2,17 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using WM.Core.Aspects.PostSharp.CacheAspects;
-using WM.Core.Aspects.PostSharp.LogAspects;
 using WM.Core.CrossCuttingConcerns.Caching.Microsoft;
-using WM.Core.CrossCuttingConcerns.Logging.Log4Net.Logger;
 using WM.Northwind.Business.Abstract.EczaneNobet;
-using WM.Northwind.DataAccess.Abstract.EczaneNobet;
 using WM.Northwind.Entities.ComplexTypes.EczaneNobet;
-using WM.Northwind.Entities.Concrete.Authorization;
-using WM.Northwind.Entities.Concrete.EczaneNobet;
 using WM.Northwind.Entities.Concrete.Enums;
-using WM.Northwind.Entities.Concrete.Optimization.EczaneNobet;
-using WM.Optimization.Abstract.Health;
 
 namespace WM.Northwind.Business.Concrete.Managers.EczaneNobet
 {
