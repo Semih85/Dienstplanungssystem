a2f1d5d06c01579e1e30fbb05838bf325def5354
Semih ÖZDAMAR <ozdamarsem85@hotmail.com>
2019-12-31 17:34:58 +0100

    tarih bazlı talep alınabilmesi eklendi

--- WM.Optimization/Entities/KisitParametre/KpTarihAraligiOrtalamaEnFazla.cs
+++ WM.Optimization/Entities/KisitParametre/KpTarihAraligiOrtalamaEnFazla.cs
@@ -5,13 +5,12 @@ using WM.Northwind.Entities.ComplexTypes.EczaneNobet;
 
 namespace WM.Optimization.Entities.KisitParametre
 {
-    public class KpTarihAraligiOrtalamaEnFazla : OptanoKisitParametreModelBase<Model, EczaneNobetTarihAralik>
+    public class KpTarihVeAltGrupBazliEnFazla : OptanoKisitParametreModelBase<Model, EczaneNobetTarihAralik>
     {
-        public List<TakvimNobetGrup> Tarihler { get; set; }
+        public TakvimNobetGrup Tarih { get; set; }
         public int GunSayisi { get; set; }
         public double OrtalamaNobetSayisi { get; set; }
         public string GunKuralAdi { get; set; }
-        public EczaneNobetGrupDetay EczaneNobetGrup { get; set; }
         public List<EczaneNobetTarihAralik> EczaneNobetTarihAralik { get; set; }
         public NobetUstGrupKisitDetay NobetUstGrupKisit { get; set; }
 
